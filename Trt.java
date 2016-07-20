c893b9ab5ceb5deeb575873c8121c43044d8c6dd
JA3358252 <salammal06@gmail.com>
2016-07-20 17:24:50 +0530

    Create Trt.java

---
+++ Trt.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+
+public class Trt {
+
+	public static void main(String[] args) {
+		Scanner scr=new Scanner(System.in);
+		System.out.println("enter no:");
+	int n=scr.nextInt();
+	if(n<0){
+		System.out.println("no is negative:"+n);
+	}
+	else 
+	{
+		System.out.println("no is positive:"+n);
+	}
+	}
+
+}
